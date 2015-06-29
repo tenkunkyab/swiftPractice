28460c2ab456881a90faa6ce658af3dbdbc57182
tenkunkyab <ten.kunkyab@gmail.com>
2015-06-29 22:52:47 +0530

    First Commit, Bare Bone

---
+++ tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tutorial
+//
+//  Created by Tenzin Kunkyab on 29/06/15.
+//  Copyright (c) 2015 Tenzin Kunkyab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
